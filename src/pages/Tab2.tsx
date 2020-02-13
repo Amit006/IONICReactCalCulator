8a01b5221f347ab74466e6e4918152969f75d093
Amit Nayek <amitnk19@gmail.com>
2020-02-14 01:18:19 +0530

    Complete Picture and gallery  using React

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
+import { camera, trash, close } from 'ionicons/icons';
+import {
+    IonFab, IonFabButton, IonIcon, IonGrid, IonRow,
+    IonCol, IonImg, IonActionSheet } from '@ionic/react';
+
+import {  usePhotoGallery } from '../hooks/usePhotoGallery';
 
 const Tab2: React.FC = () => {
+    const { photos, takePhoto } = usePhotoGallery();
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Photo Gallery</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
@@ -17,7 +24,22 @@ const Tab2: React.FC = () => {
             <IonTitle size="large">Tab 2</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        {/*<ExploreContainer name="Tab 2 page" />*/}
+          <IonGrid>
+              <IonRow>
+                  {photos.map((photo, index) => (
+                      <IonCol size="6" key={index}>
+                          <IonImg src={photo.webviewPath} />
+                          {/*<IonImg src={photo.base64 ?? photo.webviewPath} />*/}
+                      </IonCol>
+                  ))}
+              </IonRow>
+          </IonGrid>
+          <IonFab vertical="bottom" horizontal="center" slot="fixed">
+              <IonFabButton onClick={() => takePhoto()}>
+                  <IonIcon icon={camera}></IonIcon>
+              </IonFabButton>
+          </IonFab>
       </IonContent>
     </IonPage>
   );
